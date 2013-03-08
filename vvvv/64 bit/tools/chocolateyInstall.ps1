99838914fbae0b8743915f04e3a397a4fac2d5d7
smakhtin <algariol@gmail.com>
2013-03-08 16:09:44 +0400

    updated to latest vvvv

--- vvvv/64 bit/tools/chocolateyInstall.ps1
+++ vvvv/64 bit/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $binRoot = "$env:systemdrive\"
 if($env:chocolatey_bin_root -ne $null){$binRoot = $env:chocolatey_bin_root}
 $installPath = Join-Path $binRoot 'vvvv'
-$currentDir = 'vvvv_45beta29_x64'
-Install-ChocolateyZipPackage vvvv "http://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=http://vvvv.org/sites/default/files/vvvv_45beta29_x64.zip&nid=93335" $installPath
+$currentDir = 'vvvv_45beta29.2_x64'
+Install-ChocolateyZipPackage vvvv "http://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=http://vvvv.org/sites/default/files/vvvv_45beta29.2_x64.zip&nid=96038" $installPath
 $workingDir = Join-Path $installPath $currentDir
 $exeFile = Join-Path $workingDir 'vvvv.exe'
 $crack = Join-Path $workingDir 'crack.exe'
